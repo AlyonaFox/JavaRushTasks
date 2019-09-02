79ff00279080060307c36342ce93e4e1e3b5d2c1
AlyonaFox <fox@tre.one>
2019-09-02 20:43:39 +0300

    Перешли на использование Scaner Просто для теста. Это примечание.

--- 2.JavaCore/src/com/javarush/task/task15/task1531/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1531/Solution.java
@@ -1,10 +1,8 @@
 package com.javarush.task.task15.task1531;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Scanner;
 
 /* 
 Факториал
@@ -12,10 +10,8 @@ import java.math.BigInteger;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int input = Integer.parseInt(reader.readLine());
-        reader.close();
-
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
         System.out.println(factorial(input));
     }
 
